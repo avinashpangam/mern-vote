2c20678eea06cc47984c0472c6f1935f035df2ab
Avinash <pangamavinash@gmail.com>
2021-10-23 23:52:36 +0530

    post checked

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,42 +1,50 @@
-const jwt= require('jsonwebtoken');
-const db=require('../models');
+const jwt = require('jsonwebtoken');
+const db = require('../models');
 
-exports.register = async(req,res,next) =>{
-    try{
-          const user=await db.User.create(req.body);
-          const {id,username}=user;
-          const token =jwt.sign({id,username},process.env.SECRET);
-          res.status(201).json(user,username,token);
-    }catch(err){
-        if(err.code ===11000){
-            err.message='Sorry, that username is already taken';
+
+exports.register = async (req, res, next) => {
+    try {
+      const user = await db.User.create(req.body);
+      const { id, username } = user;
+      const token = jwt.sign({ id, username }, process.env.SECRET);
+  
+      return res.status(201).json({
+        id,
+        username,
+        token,
+      });
+    } catch (err) {
+      if (err.code === 11000) {
+        err.message = 'Sorry, that username is already taken';
+      }
+      return next({
+        status: 400,
+        message: err.message,
+      });
+    }
+  };
+  
+
+
+
+exports.login = async (req, res, next) => {
+    try {
+        const user = await db.User.findOne({ username: req.body.username });
+        const { id, username } = user;
+        const valid = await user.comparePassword(req.body.password);
+        if (valid) {
+            const token = jwt.sign({ id, username }, process.env.SECRET);
+            return res.json({
+                id,
+                username,
+                token
+            });
+        } else {
+            throw new Error();
         }
-        next(err);
-    }
-}
-exports.login =async(req,res,next) =>{
-    try{
-           const user =await db.User.findOne({username :req.body.username});
-           const{id,username}= user;
-           const valid =await user.comparePassword(req.body.password);
-           if(valid)
-           {
-               const token =jwt.sign({id,username},process.env.SECRET);
-               res.json({
-                   id,
-                   username,
-                   token
-               });
-           } else {
-               throw new Error();
-           } 
-    }catch(err)
-    {
-        err.message='Invalid Username/Password';
-          next(err)
+    } catch (err) {
+        err.message = 'Invalid Username/Password';
+        return next(err)
     }
 }
 
-exports.register=(req,res,next) => {}
-
-exports.login=(req,res,next) => {}
--- server/index.js
+++ server/index.js
@@ -10,10 +10,11 @@ const port=process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',(req,res) => res.json({hello : 'world'}));
 app.use('/api/auth',routes.auth);
 
 app.use(handle.notfound)
 
 app.use(handle.errors)
-app.listen(port,console.log('Server started on port ${port}'));
+app.listen(port,console.log(`Server started on port ${port}`));
